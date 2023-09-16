0a56501337c3fe20a819a4d9fb668b8789f61bce
nidhijagga <nidhijagga5239@gmail.com>
2023-09-16 16:48:50 +0530

    Added Restaurant Summary

--- src/components/UI/RestaurantImage.js
+++ src/components/UI/RestaurantImage.js
@@ -1,13 +1,15 @@
 import React from "react";
+import RestaurantSummary from "./RestaurantSummary";
 
 const RestaurantImage = () => {
   return (
-    <div className="mt-4 w-full h-96 overflow-hidden">
+    <div className="mt-4 w-full h-96 overflow-hidden relative">
       <img
         src="https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?auto=compress&cs=tinysrgb&w=2000"
         alt="Restaurant"
         className="object-cover object-center h-full w-full"
       />
+      <RestaurantSummary/>
     </div>
   );
 };
---
+++ src/components/UI/RestaurantSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RestaurantSummary = () => {
+  return (
+    <div className="absolute top-28 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-300 bg-opacity-50 rounded-lg text-center p-4">
+      <h2 className="text-2xl text-red-950 font-semibold">Where Every Bite is an Artful Creation</h2>
+      <p className="mt-2 text-white">Indulge in culinary excellence at Perfectly Cooked, where every dish is a masterpiece of flavor and presentation</p>
+      <p className='text-white'>Discover delectable dishes and flavors from around the world with our food app. Taste, savor, and explore today!</p>
+    </div>
+  );
+}
+
+export default RestaurantSummary;
